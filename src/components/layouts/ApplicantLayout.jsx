c5136ebb072979bd1933d3060a27a9f827c7ae91
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-29 23:24:16 +0530

    created applicant layout

---
+++ src/components/layouts/ApplicantLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+function ApplicantLayout() {
+  return (
+    <>
+      {/* Navbar */}
+
+      <Outlet />
+    </>
+  );
+}
+
+export default ApplicantLayout;
