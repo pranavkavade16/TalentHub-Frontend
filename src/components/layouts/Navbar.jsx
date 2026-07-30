7c97dabc626040364190cb6e53860713e9e10496
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-30 15:52:41 +0530

    created few UI changes in navbar

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,90 +1,110 @@
 import { NavLink } from "react-router-dom";
 
+import Logo from "../common/Logo";
+import SearchBar from "../common/SearchBar";
+
 const Navbar = () => {
   return (
     <nav
       className="navbar navbar-expand-lg bg-white border-bottom sticky-top"
-      style={{ height: "72px" }}
+      style={{ minHeight: "72px", zIndex: 1030 }}
     >
       <div className="container-fluid px-4">
         {/* Logo */}
 
-        <div className="d-flex align-items-center gap-3">
-          <div
-            className="bg-primary rounded-3 d-flex justify-content-center align-items-center text-white fw-bold"
-            style={{
-              width: 42,
-              height: 42,
-            }}
+        <div className="d-flex align-items-center">
+          <button
+            className="btn btn-light rounded-3 border d-lg-none me-3"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#sidebar"
           >
-            TH
-          </div>
-          <NavLink to="/">
-            <span className="fw-bold fs-4">
-              Talent<span className="text-primary">Hub</span>
-            </span>
-          </NavLink>
+            <i className="bi bi-list fs-5"></i>
+          </button>
+
+          <Logo />
         </div>
-        {/* Mobile */}
+
+        {/* Mobile Toggle */}
 
         <button
           className="navbar-toggler border-0 shadow-none"
+          type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbar"
+          data-bs-target="#navbarMenu"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbar">
+        {/* Right */}
+
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarMenu"
+        >
           {/* Search */}
 
-          <div className="flex-grow-1 mx-lg-5 my-3 my-lg-0">
-            <div className="position-relative">
-              <i className="bi bi-search position-absolute top-50 start-0 translate-middle-y ms-3 text-secondary"></i>
-
-              <input
-                type="search"
-                className="form-control rounded-pill border-0 bg-light ps-5 py-2"
-                placeholder="Search jobs, candidates..."
-              />
-            </div>
+          <div
+            className="flex-grow-1 mx-lg-5 my-3 my-lg-0"
+            style={{ maxWidth: "650px" }}
+          >
+            <SearchBar placeholder="Search jobs, candidates..." />
           </div>
 
-          {/* Right */}
-
           <div className="d-flex align-items-center gap-2">
+            {/* Notifications */}
+
+            <button className="btn btn-light border rounded-circle position-relative">
+              <i className="bi bi-bell"></i>
+
+              <span
+                className="position-absolute top-0 start-100 translate-middle p-1 bg-danger rounded-circle"
+                style={{
+                  width: "8px",
+                  height: "8px",
+                }}
+              ></span>
+            </button>
+
+            {/* Settings */}
+
+            <button className="btn btn-light border rounded-circle">
+              <i className="bi bi-gear"></i>
+            </button>
+
+            {/* Profile */}
+
             <div className="dropdown">
               <button
-                className="btn d-flex align-items-center gap-2 border-0"
+                className="btn btn-light border rounded-3 d-flex align-items-center gap-2 px-2"
                 data-bs-toggle="dropdown"
               >
                 <img
                   src="https://i.pravatar.cc/100"
-                  className="rounded-circle"
-                  width="42"
-                  height="42"
-                  alt=""
+                  alt="Profile"
+                  width="40"
+                  height="40"
+                  className="rounded-circle border"
                 />
 
-                <div className="text-start d-none d-lg-block">
+                <div className="text-start d-none d-xl-block">
                   <div className="fw-semibold small">Pranav Kavade</div>
 
-                  <small className="text-secondary">Recruiter</small>
+                  <small className="text-muted">Recruiter</small>
                 </div>
 
-                <i className="bi bi-chevron-down small"></i>
+                <i className="bi bi-chevron-down small text-muted"></i>
               </button>
 
-              <ul className="dropdown-menu dropdown-menu-end shadow-sm border-0 rounded-4 mt-3">
+              <ul className="dropdown-menu dropdown-menu-end border-0 shadow-sm rounded-3 mt-3">
                 <li>
-                  <NavLink className="dropdown-item" to="/recruiter/profile">
+                  <NavLink to="/recruiter/profile" className="dropdown-item">
                     <i className="bi bi-person me-2"></i>
-                    Profile
+                    My Profile
                   </NavLink>
                 </li>
 
                 <li>
-                  <NavLink className="dropdown-item" to="/settings">
+                  <NavLink to="/settings" className="dropdown-item">
                     <i className="bi bi-gear me-2"></i>
                     Settings
                   </NavLink>
