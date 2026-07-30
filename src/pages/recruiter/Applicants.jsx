44bbf4692849a38611f8c6737ec81986b14dee7b
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-30 16:40:33 +0530

    created a applicants pge

---
+++ src/pages/recruiter/Applicants.jsx
@@ -0,0 +1,23 @@
+import ApplicantFilters from "../../components/applicants/ApplicantFilters";
+import ApplicantTable from "../../components/applicants/ApplicantTable";
+import PageHeader from "../../components/common/PageHeader";
+import Button from "../../components/common/Button";
+
+const Applicants = () => {
+  return (
+    <>
+      <PageHeader
+        title="Applicants Management"
+        subtitle="Review and manage candidates across all active job postings."
+      >
+        <Button icon="bi-plus-lg">Post a New Job</Button>
+      </PageHeader>
+
+      <ApplicantFilters />
+
+      <ApplicantTable />
+    </>
+  );
+};
+
+export default Applicants;
