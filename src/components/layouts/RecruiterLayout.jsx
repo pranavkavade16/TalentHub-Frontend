1432a2af3f697e8c35af664e6f213effa11c203a
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-30 11:11:46 +0530

    imported and used the navbar and sidebar in RecruiterLayout

--- src/components/layouts/RecruiterLayout.jsx
+++ src/components/layouts/RecruiterLayout.jsx
@@ -1,15 +1,27 @@
 import { Outlet } from "react-router-dom";
 
-function RecruiterLayout() {
+import Navbar from "../components/layout/Navbar";
+import Sidebar from "../components/layout/Sidebar";
+import Footer from "../components/layout/Footer";
+
+const RecruiterLayout = () => {
   return (
     <>
-      {/* Navbar */}
+      <Navbar />
 
-      {/* Sidebar */}
+      <div className="container-fluid">
+        <div className="row">
+          <Sidebar />
 
-      <Outlet />
+          <main className="col-lg-10 col-md-9 col-12 bg-light p-4 min-vh-100">
+            <Outlet />
+          </main>
+        </div>
+      </div>
+
+      <Footer />
     </>
   );
-}
+};
 
 export default RecruiterLayout;
